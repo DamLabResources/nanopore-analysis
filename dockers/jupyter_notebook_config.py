700c7f0c120e529bbcc6c507b2ba54c8193f6dc0
Will Dampier <judowill@gmail.com>
2020-04-16 09:52:34 -0400

    Fixing issue with git extension. Requires docker rebuild

--- dockers/jupyter_notebook_config.py
+++ dockers/jupyter_notebook_config.py
@@ -12,6 +12,12 @@ c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 
+
+c.NotebookApp.nbserver_extensions = {
+    'jupyterlab_git': True,
+} 
+
+
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
 
